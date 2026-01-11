7576364eef4d8761234254236f03822742142bfd
nongyao2516 <nongyao.so@gmail.com>
2026-01-11 16:41:16 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,10 @@
+<template >
+    <div>
+        <h1>Contact to Nongyao Sornjapo</h1>
+        <h3>Tel.038-743-690</h3>
+         <img alt="Vue logo" src="../assets/cat.jpg">
+    </div>
+</template>
+
+
+
